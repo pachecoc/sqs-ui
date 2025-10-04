4619fbc7b7659822345a8a0f1d55dde2f812a52b
Gustavo Pacheco <pacheco.gpc@gmail.com>
2025-10-04 17:06:44 -0300

    Default commit by Pacheco

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -11,7 +11,7 @@ import (
 
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/sqs"
-	appconfig"github.com/pachecoc/sqs-ui/internal/config"
+	"github.com/pachecoc/sqs-ui/internal/config"
 	"github.com/pachecoc/sqs-ui/internal/handler"
 	"github.com/pachecoc/sqs-ui/internal/service"
 )
